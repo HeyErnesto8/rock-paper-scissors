dc37d7af8a6126971181ea0d39dae698a3599045
Ernesto <ernesto.trujillo@gmail.com>
2023-02-09 21:19:33 -0500

    Some changes

---
+++ DOMExcersice.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.style.background = "pink";
+content.style.border = "thick solid #000000";  
+container.appendChild(content);
